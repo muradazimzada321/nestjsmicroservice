d8b037fbc3596ae45b7a7f33a1534a5624163879
Murad Əzimzadə <murad.azimzade.k201@teams.dia.edu.az>
2023-01-12 22:28:37 +0400

    .

--- producer/producer/src/app.service.ts
+++ producer/producer/src/app.service.ts
@@ -5,4 +5,29 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  async scrapeData(): Promise<string[]> {
+    let context: string[];
+    await axios
+      .get('https://oxu.az', {
+        headers: { 'Accept-Encoding': 'gzip,deflate,compress' },
+      })
+      .then((res) => {
+        const $ = load(res.data);
+        const Attrubutes = $('.news-i')
+          .children('.news-i-inner')
+          .children('.news-i-content');
+        const content = Attrubutes.children('.title')
+          .toArray()
+          .map((x) => {
+            return [
+              $(x).text(),
+              $(x).parent().find('.when-time').text(),
+              $(x).parent().find('.when').children('.when-date').text(),
+            ].join(' ');
+          });
+
+        context = content;
+      });
+    return context;
+  }
 }
